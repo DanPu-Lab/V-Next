06fbd4b967a509845a417b1b66a8278c881774b1
DanPu-Lab <117810828+DanPu-Lab@users.noreply.github.com>
2025-07-24 01:15:02 +0800

    Update run_test.sh

--- test/run_test.sh
+++ test/run_test.sh
@@ -19,15 +19,15 @@ if [ ! -f Homo_sapiens.GRCh37.75.dna.chromosome.22.fa.fai ]
 then
 	samtools faidx Homo_sapiens.GRCh37.75.dna.chromosome.22.fa
 fi
-rm -rf work_standalone
-#Stand-alone V_Next test 
+rm -rf work_file
+# V_Next test 
 python ${V_Next_dir}/V_Next/python/preprocess.py \
 	--mode call \
 	--reference Homo_sapiens.GRCh37.75.dna.chromosome.22.fa \
 	--region_bed ${test_dir}/region.bed \
 	--tumor_bam ${test_dir}/tumor.bam \
 	--normal_bam ${test_dir}/normal.bam \
-	--work work_standalone \
+	--work work_file \
 	--scan_maf 0.05 \
 	--min_mapq 10 \
 	--snp_min_af 0.05 \
@@ -39,28 +39,28 @@ python ${V_Next_dir}/V_Next/python/preprocess.py \
 	--scan_alignments_binary ${V_Next_dir}/V_Next/bin/scan_alignments
 
 CUDA_VISIBLE_DEVICES= python ${V_Next_dir}/V_Next/python/call.py \
-		--candidates_tsv work_standalone/dataset/*/candidates*.tsv \
+		--candidates_tsv work_file/dataset/*/candidates*.tsv \
 		--reference Homo_sapiens.GRCh37.75.dna.chromosome.22.fa \
-		--out work_standalone \
-		--checkpoint ${V_Next_dir}/V_Next/models/NeuSomatic_v0.1.0_standalone_WEX_100purity.pth \
+		--out work_file \
+		--checkpoint ${V_Next_dir}/V_Next/models/DREAM.pth \
 		--num_threads 1 \
 		--batch_size 100
 
 python ${V_Next_dir}/V_Next/python/postprocess.py \
 		--reference Homo_sapiens.GRCh37.75.dna.chromosome.22.fa \
 		--tumor_bam ${test_dir}/tumor.bam \
-		--pred_vcf work_standalone/pred.vcf \
-		--candidates_vcf work_standalone/work_tumor/filtered_candidates.vcf \
-		--output_vcf work_standalone/V_Next_standalone.vcf \
-		--work work_standalone 
+		--pred_vcf work_file/pred.vcf \
+		--candidates_vcf work_file/work_tumor/filtered_candidates.vcf \
+		--output_vcf work_file/V_Next.vcf \
+		--work work_file 
 
 
 
 cd ..
 
-file1=${test_dir}/example/work_standalone/V_Next_standalone.vcf
-file2=${test_dir}/V_Next_standalone.vcf
+file1=${test_dir}/example/work_file/V_Next.vcf
+file2=${test_dir}/V_Next.vcf
 
-cmp --silent $file1 $file2 && echo "### V_Next stand-alone: SUCCESS! ###" \
-|| echo "### V_Next stand-alone FAILED: Files ${file1} and ${file2} Are Different! ###"
+cmp --silent $file1 $file2 && echo "### V_Next: SUCCESS! ###" \
+|| echo "### V_Next FAILED: Files ${file1} and ${file2} Are Different! ###"
 
