b8213a0e09cb38693a02831d12e2b16376fc7892
DanPu-Lab <117810828+DanPu-Lab@users.noreply.github.com>
2025-08-29 00:17:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
 
-rm -rf $DIR/others/SeqLib/ $DIR//others/seqan/
+rm -rf $DIR/others/SeqLib/ $DIR/others/seqan/
 pushd $DIR/V_Next
   mkdir build
     pushd build
